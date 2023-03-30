8f25f05bfad9da8d2342f1a2cfb2d40afc406381
Thomzes <thomz161@gmail.com>
2023-03-31 00:02:45 +0700

    updated

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const rupiah = require('rupiah-format');
 const host = "127.0.0.1";
 const port = 3002;
 
@@ -6,8 +7,32 @@ const port = 3002;
 // response = data keluar dari sistem
 
 const server = http.createServer(function (request, response) {
-    response.statusCode = 203;
-    response.end('Hello world!');
+    const nama = "Thomas Ardiansah";
+    let uang = 500000;
+    let jajan = 150000;
+    let sisa = uang - jajan;
+
+    uang = rupiah.convert(uang);
+    jajan = rupiah.convert(jajan);
+    sisa = rupiah.convert(sisa);
+
+    // const sisaRupiah = rupiah.convert(sisa);
+    // console.log('', sisaRupiah);
+
+    const hasil = `
+    <head>
+        <title>NODE JS BEGINNER</title>
+    </head>
+    <body>
+        
+        <h1 style="background: black; color: white; padding: 20px; text-align: center">Ini Beginner Node</h1>
+
+        <p>hallo nama saya ${nama} saya jajan sebanyak ${jajan}, semula uang saya ${uang}, sekarang sisa ${sisa}</p>
+
+    </body>`;
+
+    response.statusCode = 200;
+    response.end(hasil);
 });
 
 server.listen(port, host, '', function () {
