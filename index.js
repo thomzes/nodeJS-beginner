f536e37e9d474b46a9e7d12d4a4c22073b7a1f83
Tommy <thomz161@gmail.com>
2023-03-30 16:55:41 +0700

    updated

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const http = require('http');
+const host = "127.0.0.1";
+const port = 3002;
 
 // request = data masuk dari luar
 // response = data keluar dari sistem
 
 const server = http.createServer(function (request, response) {
+    response.statusCode = 203;
     response.end('Hello world!');
 });
 
-server.listen(3001, '127.0.0.1', '', function () {
-    console.log('server berhasil running');
+server.listen(port, host, '', function () {
+    console.log(`server berhasil running di host : ${host} port : ${port}`);
 });
 
 
